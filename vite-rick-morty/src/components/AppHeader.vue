4f7ab5fae19ff3d5d3e3e73d0ddbb4cf62fc6ce1
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-05-06 15:15:49 +0200

    feat: project init and AppHeader

---
+++ vite-rick-morty/src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Ricky and Morty App",
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <h1>{{ title }}</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixins" as *;
+
+header {
+  position: fixed;
+  z-index: 99;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  background-color: $header-color;
+  height: $header-height;
+
+  .container {
+    @include flex(row, center, center);
+    height: 100%;
+  }
+}
+</style>
---
+++ vite-rick-morty/src/components/AppMain.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios'
+export default{
+  data(){
+    return{
+      characterArray: [],
+    }
+  },
+  created(){
+    axios.get('https://rickandmortyapi.com/api/character').then((resp)=>{
+      this.characterArray = resp;
+      console.log(this.characterArray.data.results[0].name);
+      console.log(this.characterArray.data.results[0].status);
+      console.log(this.characterArray.data.results[0].species);
+
+
+    });
+  }
+}
+</script>
+
+<template>
+    <h1>ciao</h1>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixins" as *;
+
+
+</style>
